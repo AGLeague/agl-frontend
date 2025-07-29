fda10fdefb2edd653a931d10dc2c04edb868d26f
Stephen Heitman <saheitman@gmail.com>
2025-07-28 20:58:53 -0700

    fix refresh in production

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   base: '/agl-frontend/',
-  plugins: [react()],
-  base: '/agl-frontend/',
+  plugins: [react()]
 })
